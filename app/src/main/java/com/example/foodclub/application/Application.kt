12e69f7141753860ada4fc03b2b86e28cdf2c5c4
AmethystOwl <youssef.abdallah2015@hotmail.com>
2022-12-27 03:20:07 +0200

    Update Application.kt

--- app/src/main/java/com/example/foodclub/application/Application.kt
+++ app/src/main/java/com/example/foodclub/application/Application.kt
@@ -12,42 +12,7 @@ class Application : LocaleAwareApplication() {
             accessToken = getString(R.string.mapbox_access_token),
             locationEngine = LocationEngineProvider.getBestLocationEngine(this)
         )*/
-        /* val client = ClientSearch(
-             ApplicationID("5J6686IE08"),
-             APIKey("52f093c565f9d0255354548421c84f4f")
-         )
-         val index = client.initIndex(IndexName("menu_items"))
-         FirebaseFirestore.getInstance().collection(Constants.RESTAURANTS_COLLECTION).get()
-             .addOnCompleteListener {
-                 it.result.documents.forEach { restaurantSnapShot ->
-                     val res = restaurantSnapShot.toObject(Restaurant::class.java)!!
-                     res.id = restaurantSnapShot.id
-                     restaurantSnapShot.reference.collection(Constants.MENU_ITEMS_COLLECTION)
-                         .get().addOnCompleteListener {
-                             it.result.documents.forEach { mealSnapShot ->
-                                 val meal = mealSnapShot.toObject(MenuItem::class.java)!!
-                                 meal.id = mealSnapShot.id
-
-
-                                 val searchable = Product(
-                                     res.id!!,
-                                     ObjectID(meal.id!!),
-                                     meal.name!!,
-                                     meal.likes!!,
-                                     meal.imageUrl!!,
-                                     meal.category!!,
-                                     restaurantName = res.name!!,
-                                     restaurantImageUrl = res.iconImageUrl!!
-                                 )
-                                 GlobalScope.launch {
-                                     index.saveObject(Product.serializer(), searchable)
-                                 }
-                             }
-                         }
-                 }
-             }*/
-
-
+       
     }
 
 }
